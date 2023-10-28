288a5b9fed1d57cfac5cdc97fccd30806659c0ae
Himanikuraware <kurawarehimani26@gmail.com>
2023-10-28 17:03:47 +0530

    Implemented API with express and typescript.

--- app.ts
+++ app.ts
@@ -1,5 +1,9 @@
-const express = require('express');
+import express from 'express';
+
+import todosRoutes from './routes/todos';
 
 const app = express();
+
+app.use(todosRoutes)
 
 app.listen(3000);
