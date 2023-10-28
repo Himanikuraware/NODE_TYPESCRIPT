68f60ff2d95ba0b9e0f6b34dd4124137f55eaefd
Himanikuraware <kurawarehimani26@gmail.com>
2023-10-28 17:23:21 +0530

    Worked on put and delete API.

--- routes/todos.ts
+++ routes/todos.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { Todo } from "../models/todo";
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
@@ -16,6 +16,27 @@ router.post("/todo", (req, res, next) => {
     text: req.body.text,
   };
   todos.push(newTodo);
+  res
+    .status(201)
+    .json({ message: "Added Successfully!", todo: newTodo, todos: todos });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const tId = req.params.todoId;
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === tId);
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    return res
+      .status(200)
+      .json({ message: "Successfully updated!", todos: todos });
+  }
+  res.status(404).json({ message: "Couldn't find the item for this id." });
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  const tId = req.params.todoId;
+  todos = todos.filter((todoItem) => todoItem.id !== tId);
+  res.json({ message: "Item deleted successfully!", todos: todos });
 });
 
 export default router;
